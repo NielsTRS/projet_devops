31b94b311738261e96d976be41c15d53b0fa6a61
alice <alice@vett>
2025-04-09 08:47:23 +0200

    View of the DataFrame

---
+++ src/main/java/com/example/View.java
@@ -0,0 +1,124 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class View {
+    private Dataframe dataframe;
+    public View(Dataframe dataframe) {
+        this.dataframe = dataframe;
+    }
+
+    public void allDataframe(Dataframe dataframe) {
+        int size = 20;
+
+        //View of the labels
+        System.out.print("  ");
+        List<String> labels = dataframe.getColumnLabels();
+        for (String label : labels) {
+            System.out.print(label + " ");
+            for (int j=0;j<size-label.length();j++) {
+                System.out.print(" ");
+            }
+        }
+
+        //View of the rest of the DataFrame
+        System.out.println();
+        for (int i=0;i<dataframe.getRowCount();i++) {
+            List<Object> row = dataframe.getRow(i);
+            System.out.print(i+ " ");
+            for (int j=0;j<row.size();j++) {
+                Object value = row.get(j);
+                System.out.print(value+" ");
+                for (int k=0;k<size-value.toString().length();k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void firstRowDataframe(Dataframe dataframe, int nbRows) {
+        int size = 20;
+        int index = 5;
+
+        //View of the labels
+        System.out.print("       ");
+        List<String> labels = dataframe.getColumnLabels();
+        for (String label : labels) {
+            System.out.print(label + " ");
+            for (int j=0;j<size-label.length();j++) {
+                System.out.print(" ");
+            }
+        }
+
+        //View of the rest of the DataFrame
+        System.out.println();
+        for (int i=0;i<nbRows;i++) {
+            List<Object> row = dataframe.getRow(i);
+            System.out.print(i);
+            for (int j=0;j<row.size();j++) {
+                Object value = row.get(j);
+                System.out.print(value+" ");
+                for (int k=0;k<size-value.toString().length();k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void lastRowDataframe(Dataframe dataframe, int nbRows) {
+        int size = 20;
+
+        //View of the labels
+        System.out.print("  ");
+        List<String> labels = dataframe.getColumnLabels();
+        for (String label : labels) {
+            System.out.print(label + " ");
+            for (int j=0;j<size-label.length();j++) {
+                System.out.print(" ");
+            }
+        }
+
+        //View of the rest of the DataFrame
+        System.out.println();
+        for (int i=nbRows;i<dataframe.getRowCount();i++) {
+            List<Object> row = dataframe.getRow(i);
+            System.out.print(i+ " ");
+            for (int j=0;j<row.size();j++) {
+                Object value = row.get(j);
+                System.out.print(value+" ");
+                for (int k=0;k<size-value.toString().length();k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> columnLabels = Arrays.asList("Name", "Age", "Profession");
+        Dataframe dataframe = new Dataframe(columnLabels);
+        Map<String, Object> row1 = new HashMap<>();
+        row1.put("Name", "Athéna");
+        row1.put("Age", 23);
+        row1.put("Profession", "Engineer");
+        dataframe.addRow(row1);
+
+        Map<String, Object> row2 = new HashMap<>();
+        row2.put("Name", "Briséis");
+        row2.put("Age", 30);
+        row2.put("Profession", "Doctor");
+        dataframe.addRow(row2);
+
+
+        View view = new View(dataframe);
+        view.allDataframe(dataframe);
+        view.firstRowDataframe(dataframe,1);
+        view.lastRowDataframe(dataframe,1);
+
+    }
+}
